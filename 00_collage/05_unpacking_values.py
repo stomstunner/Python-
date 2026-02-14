c9270da8c8f82970d9c2ae1fa0b9ac805654fa53
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 19:11:08 +0530

    first commit

---
+++ 00_collage/05_unpacking_values.py
@@ -0,0 +1,27 @@
+# if we have a sequence of data then we can store it in multiple variable at one line
+
+odd_num = [1,3,5,]
+
+# now i can store each values to the diffrent varibales
+
+# a , b, c = odd_num
+# print(a,b,c)
+
+# # =======================
+
+# a = odd_num
+# print(a)
+
+# +================================
+
+even_num = [2,4,6]
+
+# a , b = even_num[0,1]
+# not working
+
+a,b= even_num[0:2]
+print(a)
+print(b)
+print(a,b)
+
+
