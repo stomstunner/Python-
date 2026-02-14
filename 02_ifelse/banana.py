496a264f2c0f920c2b9d07bda1556561393c36c9
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 19:15:13 +0530

    first commit

---
+++ 02_ifelse/banana.py
@@ -0,0 +1,32 @@
+fruit = input("Enter the fruit name: ")
+color = input("Enter the color name: ")
+
+if fruit =="banana":
+    if color == "green":
+        print("Unripe")
+    elif color == "yellow":
+        print("Ripe")
+    elif  color == "brown":
+        print("Overripe")
+    else:
+        print("Enter a valid color name!")
+elif fruit == "guava":
+    if color == "green":
+        print("Unripe")
+    elif color == "yellow":
+        print("Ripe")
+    elif  color == "brown":
+        print("Overripe")
+    else:
+        print("Enter a valid color name!")
+elif fruit == "mango":
+    if color == "green":
+        print("Unripe")
+    elif color == "yellow":
+        print("Ripe")
+    elif  color == "brown":
+        print("Overripe")
+    else:
+        print("Enter a valid color name!")
+else:
+    print("Enter a valid fruit name")
