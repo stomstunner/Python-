b015eef480ea2ae430fb0bd24c7499eee5fcb0bd
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 19:14:44 +0530

    first commit

---
+++ 01_Basic/hello_world.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+def hello(n):
+    print(n)
+
+# hello("Hello")
+
+# hello_one = "Ujjwal"
+# hello_two = "nirmal"
+# hello_three = "prince"
+
+# import pandas
+
+
+# lets see the diffrent types of showing things in the displaying techniques
+# 1 in terminal
+repr('ujjwal')
+# 2
+str('ujjwal')
+# 3
+print('ujjwal')
