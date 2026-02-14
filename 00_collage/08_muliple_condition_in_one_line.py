17b19509f604d9797f3ecd43bfb2d537733b2674
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 19:11:52 +0530

    first commit

---
+++ 00_collage/08_muliple_condition_in_one_line.py
@@ -0,0 +1,9 @@
+# a = 330
+# b = 330
+# print("A") if a > b else print("equal") if a == b else print("B")
+
+
+username = ""
+
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
