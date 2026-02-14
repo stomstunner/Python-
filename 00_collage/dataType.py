6d3c8e9a72d2e7d06fa494bc2e7001b264fbe612
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 19:13:50 +0530

    first commit

---
+++ 00_collage/dataType.py
@@ -0,0 +1,6 @@
+print("Floor Operator", 10//3,"\n")
+print("Modulas Operator", 10%3,"\n")
+print("Arithimetic Operator", 2+3,3-1,4*5,10/4,90%12,"\n")
+print("Logical Operator", 12 == 13, 12>13, 13>12 , 12!=12,True == 1,"\n")
+
+
