30ae86c7d6849adffec9a9ccb70d0136ea51ad38
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-14 19:12:02 +0530

    first commit

---
+++ 00_collage/09_classProgram.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the number: "))
+# x = int(input("Enter the limit of table: "))
+a = 1
+
+while a <= 10:
+    print(num ,"X",a ,"=" ,num*a)
+    a = a+1
+else:
+    print("The table can't be print more than 10th term")
+
