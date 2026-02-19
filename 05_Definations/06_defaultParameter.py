c9d0c84ff6163deffb967204e64d4df8884bf3ba
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-19 18:12:55 +0530

    first commit

---
+++ 05_Definations/06_defaultParameter.py
@@ -0,0 +1,38 @@
+# in this program we write the code when we call a defination and if we did not give the argument then also it return a some value
+
+# the parameter ke ander ka vlaue only used when we do not have any arguments
+
+def greet(name = "Ujjwal"):
+    return "Hello, "+ name + " !!"
+
+name = input("Enter your name: ")
+print("With argument: ",greet(name))
+print("Without argument: ",greet())    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
