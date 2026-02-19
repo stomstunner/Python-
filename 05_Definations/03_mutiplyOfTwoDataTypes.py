01bc6c993bf8454b526e6e41ec08bb5ee1633eae
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-19 18:11:33 +0530

    first commit

---
+++ 05_Definations/03_mutiplyOfTwoDataTypes.py
@@ -0,0 +1,10 @@
+# here we want to write a program where the defination supports polymorphism where if the parameter/placeholder have diffrent type of datatype then it also should multiply things
+
+def sum_two_num(num1,num2):
+    return num1*num2
+
+print(sum_two_num('ujjwal',5))
+print(sum_two_num('u',5))
+print(sum_two_num(5,5))
+print(sum_two_num(5,'ujjwal'))
+
