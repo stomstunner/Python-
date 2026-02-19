e41d137d1b08b3c520f9b9e2f6389a1a97f86f87
Ujjwal Kumar <ujjwalk863@gmail.com>
2026-02-19 18:11:14 +0530

    first commit

---
+++ 05_Definations/01_squareNum.py
@@ -0,0 +1,8 @@
+# here we are going to program of suare of a number by using definations
+def square(number):
+    return number**2
+
+n = int(input("Enter a number: "))
+result = square(n)
+print("The square of",n,"is:",result)
+    
